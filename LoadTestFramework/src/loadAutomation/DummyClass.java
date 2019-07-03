e3ae5ec485dcbd2d3f7d049d8791ca7261486e57
Iliyas Hussain <iliyas.hussain@iliyas.apple.com>
2019-07-03 15:29:56 +0530

    forked from master

--- LoadTestFramework/src/loadAutomation/DummyClass.java
+++ LoadTestFramework/src/loadAutomation/DummyClass.java
@@ -1,5 +1,9 @@
 package loadAutomation;
 
+import java.*;
+
 public class DummyClass {
-	System.out.Println("hello world!!!!");	
+	System.out.Println("hello world!!!!");
+	
+	System.out.Println("how are you all doing");
 }
